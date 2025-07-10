f37df0aaf440509124f67d039e2d7baa3590e9c2
Calebe Lino <calebe.n.lino@gmail.com>
2025-07-10 16:26:51 +0000

    trying to adapt tests

--- java-native-game/src/test/java/com/calebe/engine/CycleManagerTest.java
+++ java-native-game/src/test/java/com/calebe/engine/CycleManagerTest.java
@@ -1,12 +1,47 @@
 package com.calebe.engine;
 
+import com.calebe.engine.game.Attribute;
+import com.calebe.engine.game.GameObject;
+import com.calebe.engine.game.Script;
 import org.junit.jupiter.api.BeforeAll;
 import com.calebe.engine.DaggerTestEngineComponent;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CycleManagerTest {
-    @BeforeAll
-    static void setup() {
+    @Test
+    void test_scripts_activation() {
+        Script script = Mockito.mock(Script.class);
+        GameObject gameObject = Mockito.mock(GameObject.class);
+
+        Attribute<String> nameAttr = new Attribute<>("name", "");
+        Mockito.when(script.update()).thenReturn(() -> new Script.Update() {
+            {
+                name = script.gameAttribute(nameAttr.key, nameAttr.value);
+            }
+            Attribute<String> name;
+            @Override
+            public void execute() {
+
+            }
+        });
+        Mockito.when(gameObject.getAttributes()).thenReturn(Set.of(nameAttr));
+        Mockito.when(gameObject.getScripts()).thenReturn(Set.of(script));
+
+        CycleManager cycleManager = DaggerTestEngineComponent.factory()
+                .create(new TestEngineContext(Set.of(gameObject), Set.of(script)))
+                .cycleManager();
+
+        cycleManager.performActivation();
+        assertNotNull(cycleManager.sharedAttributes.get(script));
+        assertEquals(gameObject, cycleManager.sharedAttributes.get(script).stream()
+                .findFirst().map(Map.Entry::getKey).orElseThrow());
+        assertEquals(nameAttr, cycleManager.sharedAttributes.get(script).stream()
+                .findFirst().map(Map.Entry::getValue).map(m -> m.get(nameAttr.key)).orElseThrow());
     }
 }
--- java-native-game/src/test/java/com/calebe/engine/TestEngineContext.java
+++ java-native-game/src/test/java/com/calebe/engine/TestEngineContext.java
@@ -4,7 +4,29 @@ import com.calebe.engine.game.GameObject;
 import com.calebe.engine.game.Script;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
-public record TestEngineContext(Set<GameObject> gameObjects, Set<Script> scripts) {
+public final class TestEngineContext {
+    private final Set<GameObject> gameObjects;
+    private final Set<Script> scripts;
+
+    public TestEngineContext() {
+        this.gameObjects = new HashSet<>();
+        this.scripts = new HashSet<>();
+    }
+
+    public TestEngineContext(Set<GameObject> gameObjects, Set<Script> scripts) {
+        this();
+        this.gameObjects.addAll(gameObjects);
+        this.scripts.addAll(scripts);
+    }
+
+    public Set<GameObject> gameObjects() {
+        return this.gameObjects;
+    }
+
+    public Set<Script> scripts() {
+        return this.scripts;
+    }
 }
