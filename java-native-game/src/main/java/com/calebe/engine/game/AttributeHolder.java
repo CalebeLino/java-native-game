3912e1e8fcbe8ab37f66ec683ee21d2f71384175
Calebe Lino <calebe.n.lino@gmail.com>
2025-07-11 14:33:18 +0000

    attribute creation and recovery as a generic type

--- java-native-game/src/main/java/com/calebe/engine/game/AttributeHolder.java
+++ java-native-game/src/main/java/com/calebe/engine/game/AttributeHolder.java
@@ -8,21 +8,19 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public abstract class AttributeHolder {
-    Map<String, Attribute<String>> attributes = new HashMap<>();
-    public Attribute<String> gameAttribute(String key, String initialValue) {
-        if(attributes.containsKey(key)) {
-            return attributes.get(key);
-        }
-        Attribute<String> attr = new Attribute<>(key, initialValue);
-        attributes.put(key, attr);
-        return attr;
+    Map<String, Attribute<?>> attributes = new HashMap<>();
+
+    @SuppressWarnings("unchecked")
+    public <T> Attribute<T> gameAttribute(String key, T initialValue) {
+        Attribute<?> attr = attributes.computeIfAbsent(key, k -> new Attribute<>(key, initialValue));
+        return (Attribute<T>) attr;
     }
 
     public Set<Attribute<String>> getAttributes() {
         return new HashSet<>(attributes.values());
     }
 
-    public void setAttributes(Map<String, Attribute<String>> attributes) {
-        this.attributes = attributes;
+    public void setAttributes(Map<String, Attribute<?>> attributes) {
+        this.attributes.putAll(attributes);
     }
 }
